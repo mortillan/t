67e033de774da96b3f25cc1eda1ebbb56d1c90cf
Marieto Ortillan Jr <marieto.ortillan@gmail.com>
2018-08-24 18:51:23 +0800

    Nightly build

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { select, path } from 'd3';
 class App extends Component {
   render() {
     const container = select('#root').append('svg').attr('viewBox', '0 0 100 100').attr('preserveAspectRatio', 'xMidYMid meet');
-    const rect = container.append('rect').attr('x', 0).attr('y', 0).attr('width', '100').attr('height', 1).attr('fill', '#e4e4e4');
+    const rect = container.append('rect').attr('x', 0).attr('y', 0).attr('width', '100').attr('height', '2').attr('fill', '#e4e4e4');
     return (
       <div>
 
