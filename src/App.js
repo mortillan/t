1b1b52807643d608477a94df3335d517ebf82dec
Marieto Ortillan Jr <marieto.ortillan@gmail.com>
2018-08-29 18:06:13 +0800

    Nightly build

--- src/App.js
+++ src/App.js
@@ -8,30 +8,53 @@ class App extends Component {
     super();
     this.state = {
       markerX: 0,
-    }
+      tasks: [],
+    };
+    this.addTask = this.addTask.bind(this);
+  }
+
+  addTask() {
+    this.setState({tasks: [...this.state.tasks, {length: '25', start: this.state.markerX}]});
   }
 
   componentDidMount() {
-    const currentDateTime = new Date().toLocaleTimeString('en-US', { hour12: false });
+    const currentDateTime = new Date();
+    const hours = currentDateTime.getHours();
+    const minutes = currentDateTime.getMinutes();
+    const seconds = currentDateTime.getSeconds();
+    const total = ((hours * 60 * 60) + (minutes * 60) + seconds) / 100;
+
+    console.log(total);
+
+    this.setState({markerX: total});
+
     setInterval(() => {
       this.setState((prevState) => {
-        return {markerX: prevState.markerX ++}
+        console.log(prevState.markerX + 0.1 );
+        return { markerX: prevState.markerX + 0.1 };
       });
     }, 1000);
   }
 
   render() {
-    // const container = select('#root').append('svg').attr('viewBox', '0 0 100 100').attr('preserveAspectRatio', 'xMidyMid meet');
-    // const rect = container.append('rect').attr('x', '0').attr('y', '0.5').attr('width', '100').attr('height', '2').attr('fill', '#e4e4e4');
-    // const task1 = container.append('rect').attr('x', '24').attr('y', '0.5').attr('width', '20').attr('height', '2').attr('fill', 'blue');
-    // container.append('rect').attr('ref', ).attr('fill', 'red').attr('x', '50').attr('y', '0').attr('height', '3').attr('width', '0.05');
     return (
-      <svg viewBox='0 0 100 100'>
-        <rect x='0' y='0.5' width='86400' height='2' fill='#e4e4e4' />
-        <rect x={this.state.markerX} y='0' width='0.05' height='3' fill='red' />
-      </svg>
+      <div>
+        <svg viewBox='0 0 864 100'>
+          <rect x='0' y='1.5' width='864' height='20' fill='#e4e4e4' />
+          <text x='0' y={27} fontFamily="sans-serif" fontSize='5px'>12AM</text>
+          <text x={864 / 2} y={27} fontFamily="sans-serif" fontSize='5px'>12PM</text>
+          <rect x={this.state.markerX} y='1.5' width='1' height='20' fill='red' />
+          <rect x={this.state.markerX + 0.1} y='1.5' width={864 - this.state.markerX + 0.1 } height='20' fill='white' />
+          {this.state.tasks.map(task => <TaskBar length={task.length} start={task.start} />)}
+        </svg>
+        <button onClick={this.addTask}>Add Task</button>
+      </div>
     );
   }
+}
+
+const TaskBar = (props) => {
+  return <rect x={props.start} y='1.5' width={props.length} height='20' fill='blue' />
 }
 
 export default App;
