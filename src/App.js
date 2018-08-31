416e9c650da71a3c438ee3275d317bc4f45340c1
Marieto Ortillan Jr <marieto.ortillan@gmail.com>
2018-08-31 18:59:37 +0800

    Nightly build

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 // import { select, path } from 'd3';
+import 'bulma-slider/dist/js/bulma-slider';
 
 class App extends Component {
   constructor() {
@@ -37,7 +38,6 @@ class App extends Component {
       markerX: total,
       remainingHours: 23 - hours,
       remainingMins: 60 - minutes,
-      remainingSec: 60 - seconds,
     });
 
     setInterval(() => {
@@ -47,13 +47,11 @@ class App extends Component {
         const currentDateTime = new Date();
         const hours = currentDateTime.getHours();
         const minutes = currentDateTime.getMinutes();
-        const seconds = currentDateTime.getSeconds();
 
         return { 
           markerX: prevState.markerX + 0.01,
           remainingHours: 23 - hours,
           remainingMins: 60 - minutes,
-          remainingSec: 60 - seconds,
         };
       });
     }, 1000);
@@ -61,64 +59,75 @@ class App extends Component {
 
   render() {
     return (
-      <div>
+      <div className='vfull'>
         <nav className='navbar is-fixed-top' role='navigation' aria-label='main navigation'>
           <div className='navbar-brand'>
             <a className='navbar-item' href='https://bulma.io'>
-              <img src='' alt='' width='112' height='28' />
-              Legato
-            </a>
-
-            <a role='button' className='navbar-burger' aria-label='menu' aria-expanded='false'>
-              <span aria-hidden='true'></span>
-              <span aria-hidden='true'></span>
-              <span aria-hidden='true'></span>
+              <img src='https://via.placeholder.com/40x40' alt='' width='40' height='40' />
+              <div>Legato</div>
             </a>
           </div>
-          <div className='navbar-end'>
-            <div className='navbar-item'>
-              <div className='field is-grouped'>
-                <p className='control'>
-                  <a className='button is-link' href="#">Login</a>
-                </p>
-                <p className='control'>
-                  <a className='button is-primary' href='https://github.com/jgthms/bulma/releases/download/0.7.1/bulma-0.7.1.zip'>
-                    Create a free account
-                  </a>
-                </p>
+          <div id="navbarExampleTransparentExample" class="navbar-menu">
+            <div className='navbar-start'>
+            </div>
+            <div style={{margin: 'auto', display: 'flex', alignItems: 'strect'}}>
+              <div>4,999 online users</div>
+            </div>
+            <div className='navbar-end'>
+              <div className='navbar-item'>
+                <div className='field is-grouped'>
+                  <p className='control'>
+                    <a className='button' href="#">Login</a>
+                  </p>
+                  <p className='control'>
+                    <a className='button is-outlined' href='#'>
+                      Create a free account
+                    </a>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </nav>
-        <section className='section'>
-          <div className='columns is-vcentered'>
-            <div className='column is-12'>
-              You have {this.state.remainingHours} hours, {this.state.remainingMins} minutes, {this.state.remainingSec} seconds 
-            </div>
-          </div>
-            <div className='column is-12'>
-              <svg viewBox='0 0 864 15'>
-                <TimeBar />
-                {/* <text x='0' y={27} fontFamily='sans-serif' fontSize='5px'>12AM</text> */}
-                {/* <text x={864 / 2} y={27} fontFamily='sans-serif' fontSize='5px'>12PM</text> */}
-                <rect x={this.state.markerX + 0.01} y='1.5' width={864 - this.state.markerX + 0.01 } height='20' fill='white' />
-                {this.state.tasks.map((task, index) => <TaskBar key={index} length={task.length} start={task.start} />)}
-                <rect x={this.state.markerX} y='1.5' width='0.5' height='20' fill='red' />
-              </svg>
-            </div>
-            <div className='column is-12'>
-              <button onClick={this.addTask}>#work</button>
-              <button onClick={this.addTask}>#play</button>
-              <button onClick={this.addTask}>#break</button>
-              <button onClick={this.addTask}>+</button>
+        <section className='section vfull' style={{paddingLeft: '6rem', paddingRight: '6rem'}}>
+          <div className='columns is-vcentered vfull'>
+            <div className='column columns is-multiline'>
+              <div className='column is-12 is-size-3'>
+                You have {this.state.remainingHours} hours, {this.state.remainingMins} minutes. Enjoy the day.
+              </div>
+              <div className='column is-12'>
+                <svg viewBox='0 0 864 15'>
+                  <TimeBar />
+                  {/* <text x='0' y={27} fontFamily='sans-serif' fontSize='5px'>12AM</text> */}
+                  {/* <text x={864 / 2} y={27} fontFamily='sans-serif' fontSize='5px'>12PM</text> */}
+                  {/* <rect x={this.state.markerX + 0.01} y='1.5' width={864 - this.state.markerX + 0.01 } height='20' fill='white' /> */}
+                  {this.state.tasks.map((task, index) => <TaskBar key={index} length={task.length} start={task.start} />)}
+                  <rect x={this.state.markerX} y='1.5' width='0.5' height='20' fill='red' />
+                </svg>
+              </div>
+              <div className='column is-12'>
+                <button className='button is-outlined' onClick={this.addTask}>#work</button>
+                <button className='button is-outlined' onClick={this.addTask}>#play</button>
+                <button className='button is-outlined' onClick={this.addTask}>#break</button>
+                <button className='button is-outlined' onClick={this.addTask}>+</button>
+              </div>
             </div>
           </div>
         </section>
         <footer className='footer'>
-          <div className='content has-text-centered'>
-            <p>
+          <div className='content' style={{display: 'flex', justifyContent: 'space-between'}}>
+            <div>
               Copyright {new Date().getFullYear()} <strong>Godspeed</strong>. All rights reserverd.
-            </p>
+            </div>
+            <div style={{width: '240px'}}>
+              <input className="slider is-fullwidth" step="1" min="0" max="100" value="50" type="range" style={{margin: '0'}} />
+            </div>
+            <div>
+              <button className='button btn-circle'></button>
+              <span className="icon">
+                <i className='ion-ionic ion-help'></i>
+              </span>
+            </div>
           </div>
         </footer>
       </div>
@@ -134,7 +143,7 @@ const TaskBar = (props) => {
 
 const TimeBar = (props) => {
   return (
-    <rect x='0' y='1.5' width='864' height='20' fill='#e4e4e4' />
+    <rect x='0' y='1.5' width='864' height='20' fill='#212529' style={{fillOpacity: '.16'}} />
   );
 }
 
