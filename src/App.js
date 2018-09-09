341f270dde0904dd4e4b0089663ad093e853826b
Marieto Ortillan Jr <marieto.ortillan@gmail.com>
2018-09-09 20:47:55 +0800

    Nightly build

--- src/App.js
+++ src/App.js
@@ -18,10 +18,13 @@ class App extends Component {
       currentTaskEnd: 0,
       slider: 5,
       focuseMode: false,
+      mode: '',
     };
     this.addTask = this.addTask.bind(this);
     this.onChangeSliderValue = this.onChangeSliderValue.bind(this);
     this.onClickStopFocusMode = this.onClickStopFocusMode.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.startTime = this.startTime.bind(this);
   }
 
   addTask(e) {
@@ -55,15 +58,60 @@ class App extends Component {
       }],
       currentTaskEnd: this.state.currentTaskEnd === 0 ? this.state.markerX + length : this.state.currentTaskEnd + length,
       focuseMode: !this.state.focuseMode,
+      mode: type,
     });
   }
 
-  onClickStopFocusMode(e) {
+  startTime(e) {
+    const { target } = e;
+
+    const { type }  = target.dataset;
+
+    let color;
+
+    switch(type) {
+      case 'work':
+        color = 'red';
+        break;
+      case 'play':
+        color = 'blue';
+        break;
+      default:
+      case 'break':
+        color ='gray';
+        break;
+    }
+
+    const length = (this.state.slider * 60) / 100;
+
+    this.setState({
+      currentTask: {
+        length: length, 
+        start: this.state.markerX,
+        color: color,
+        type: type,
+      },
+      currentTaskEnd: this.state.currentTaskEnd === 0 ? this.state.markerX + length : this.state.currentTaskEnd + length,
+      focuseMode: !this.state.focuseMode,
+      mode: type,
+    });
+  }
+
+  onClickStopFocusMode() {
     if(this.state.focuseMode) {
       //TODO stop current task on curren time
       this.setState({
         focuseMode: !this.state.focuseMode,
+        currentTask: null,
       });
+    }
+  }
+
+  handleKeyPress(e) {
+    const { key } = e;
+
+    if(key === 'Escape') {
+      this.onClickStopFocusMode();
     }
   }
 
@@ -74,7 +122,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    document.addEventListener('keydown', this.onClickStopFocusMode);
+    document.addEventListener('keydown', this.handleKeyPress);
 
     const currentDateTime = new Date();
     const hours = currentDateTime.getHours();
@@ -87,10 +135,14 @@ class App extends Component {
     this.setState({
       markerX: total,
       remainingHours: 23 - hours,
-      remainingMins: 60 - minutes,
+      remainingMins: 59 - minutes,
     });
 
     setInterval(() => {
+      if(this.state.currentTask && this.state.markerX === this.state.currentTask.start + this.state.currentTask.length) {
+        this.onClickStopFocusMode();
+      }
+
       this.setState((prevState) => {
         const currentDateTime = new Date();
         const hours = currentDateTime.getHours();
@@ -104,7 +156,7 @@ class App extends Component {
           };  
         }
 
-        console.log(prevState.markerX + 0.01);
+        //console.log(prevState.markerX + 0.01);
 
         return { 
           markerX: prevState.markerX + 0.01,
@@ -122,14 +174,14 @@ class App extends Component {
           <div className={this.state.focuseMode ? 'navbar-brand invisible' : 'navbar-brand'}>
             <a className='navbar-item' href='https://bulma.io'>
               <img src='https://via.placeholder.com/40x40' alt='' width='40' height='40' />
-              <div>Legato</div>
             </a>
+            <span>Legato</span>
           </div>
           <div className="navbar-menu">
             <div className={this.state.focuseMode ? 'navbar-start invisible' : 'navbar-start'}>
             </div>
             <div style={{margin: 'auto', display: 'flex', alignItems: 'strect'}}>
-              <div>4,999 online users</div>
+              {/* <div>4,999 online users</div> */}
             </div>
             <div className={this.state.focuseMode ? 'navbar-end invisible' : 'navbar-end'}>
               <div className='navbar-item'>
@@ -165,11 +217,17 @@ class App extends Component {
                 </svg>
               </div>
               <div className='column is-12'>              
-                <button className={!this.state.focuseMode ? 'button is-outlined btn-tasks' : 'button is-outlined btn-tasks hide'} data-type='work' onClick={this.addTask}>#work</button>
-                <button className={!this.state.focuseMode ? 'button is-outlined btn-tasks' : 'button is-outlined btn-tasks hide'} data-type='play' onClick={this.addTask}>#play</button>
-                <button className={!this.state.focuseMode ? 'button is-outlined btn-tasks' : 'button is-outlined btn-tasks hide'} data-type='break' onClick={this.addTask}>#break</button>
-                <div className={this.state.focuseMode ? '' : 'hide'}>
-                  <span onClick={this.onClickStopFocusMode} className='icon'><i className='ion-ionic ion-md-close'></i></span> Press "Esc" to stop
+                <button className={!this.state.focuseMode ? 'button is-outlined btn-tasks' : 'button is-outlined btn-tasks hide'} data-type='work' onClick={this.startTime}>#work</button>
+                <button className={!this.state.focuseMode ? 'button is-outlined btn-tasks' : 'button is-outlined btn-tasks hide'} data-type='play' onClick={this.startTime}>#play</button>
+                <button className={!this.state.focuseMode ? 'button is-outlined btn-tasks' : 'button is-outlined btn-tasks hide'} data-type='break' onClick={this.startTime}>#break</button>
+                <div className={this.state.focuseMode ? 'focus-controls' : 'hide'}>
+                  <div>
+                    <span onClick={this.onClickStopFocusMode} className='icon stop'><i className='ion-ionic ion-md-close'></i></span>
+                    <span>Press "Esc" to stop</span>
+                  </div>
+                  <div>
+                    { '#' + this.state.mode }
+                  </div>
                 </div>
               </div>
             </div>
