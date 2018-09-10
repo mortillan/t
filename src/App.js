9ca5438144f05b04d1d871292a7d00f022189624
Marieto Ortillan Jr <marieto.ortillan@gmail.com>
2018-09-10 22:56:15 +0800

    Nightly build

--- src/App.js
+++ src/App.js
@@ -139,11 +139,11 @@ class App extends Component {
     });
 
     setInterval(() => {
-      this.setState((prevState) => {
-        if(this.state.currentTask && this.state.markerX >= this.state.currentTask.start + this.state.currentTask.length) {
-          return this.onClickStopFocusMode();
-        }
+      if(this.state.currentTask &&  this.state.markerX === this.state.currentTask.start + this.state.currentTask.length) {
+        this.onClickStopFocusMode();
+      }
 
+      this.setState((prevState) => {
         const currentDateTime = new Date();
         const hours = currentDateTime.getHours();
         const minutes = currentDateTime.getMinutes();
@@ -208,11 +208,15 @@ class App extends Component {
               <div className='column is-12'>
                 <svg viewBox='0 0 864 30'>
                   <TimeBar />
+                  {/* <text x='0' y={27} fontFamily='sans-serif' fontSize='5px'>12AM</text> */}
+                  {/* <text x={864 / 2} y={27} fontFamily='sans-serif' fontSize='5px'>12PM</text> */}
+                  {/* <rect x={this.state.markerX + 0.01} y='1.5' width={864 - this.state.markerX + 0.01 } height='20' fill='white' /> */}
+                  {/* {this.state.tasks.map((task, index) => <TaskBar key={index} length={task.length} fill={task.color} start={task.start} />)} */}
                   {this.state.currentTask ? <TaskBar length={this.state.currentTask.length} fill={this.state.currentTask.color} start={this.state.currentTask.start} /> : null}
                   <rect x={this.state.markerX} y='2.5' width='0.75' height='25' fill='#212529' />
                 </svg>
               </div>
-              <div className='column is-12'>              
+              <div className='column is-12' style={{ minHeight: '75px'}}>              
                 <button className={!this.state.focuseMode ? 'button is-outlined btn-tasks' : 'button is-outlined btn-tasks hide'} data-type='work' onClick={this.startTime}>#work</button>
                 <button className={!this.state.focuseMode ? 'button is-outlined btn-tasks' : 'button is-outlined btn-tasks hide'} data-type='play' onClick={this.startTime}>#play</button>
                 <button className={!this.state.focuseMode ? 'button is-outlined btn-tasks' : 'button is-outlined btn-tasks hide'} data-type='break' onClick={this.startTime}>#break</button>
