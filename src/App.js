b0ffbf64c6f1f41a05a42f5fd92d277e17a8b4fa
kian <Marieto Ortillan Jr>
2018-09-17 18:58:20 +0800

    Nightly build

--- src/App.js
+++ src/App.js
@@ -13,19 +13,20 @@ class App extends Component {
       remainingMins: 0,
       remainingSec: 0,
       markerX: 0,
-      tasksLog: [],
+      tasksLog: JSON.parse(localStorage.getItem('logs')) || {},
       currentTask: null,
       currentTaskEnd: 0,
       slider: 5,
-      focuseMode: false,
+      focusMode: false,
       mode: '',
       taskTimer: null,
       nightMode: false,
     };
     this.onChangeSliderValue = this.onChangeSliderValue.bind(this);
-    this.onClickStopFocusMode = this.onClickStopFocusMode.bind(this);
+    this.stopTimer = this.stopTimer.bind(this);
     this.handleKeyPress = this.handleKeyPress.bind(this);
     this.startTime = this.startTime.bind(this);
+    this.toggleTheme = this.toggleTheme.bind(this);
   }
 
   startTime(e) {
@@ -53,48 +54,65 @@ class App extends Component {
 
     const length = (this.state.slider * 60) / 100;
 
+    //create timer for task
     const intervalId = setInterval(() => {
       if(this.state.currentTask.remainingSec <= 0){
         return clearInterval(this.state.taskTimer);
       }
+
+      const sec = this.state.currentTask.remainingSec - 1;
       
       this.setState({
         currentTask: {
           ...this.state.currentTask,
-          remainingSec: this.state.currentTask.remainingSec - 1
+          remainingSec: sec,
         }
       });
+      document.title = `${Math.trunc(sec / 60)}:${sec % 60}`;
     }, 1000);
 
+    //save task to state
+    const newTask = {
+      length: length, 
+      start: this.state.markerX,
+      color: color,
+      type: type,
+      remainingSec: this.state.slider * 60,
+    };
+
+    const { tasksLog } = this.state;
+    const key = taskKey(new Date());
+    if(!tasksLog[key]){
+      tasksLog[key] = [];
+    }
+    tasksLog[key].push(newTask);
+
     this.setState({
-      currentTask: {
-        length: length, 
-        start: this.state.markerX,
-        color: color,
-        type: type,
-        remainingSec: this.state.slider * 60,
-      },
+      currentTask: newTask,
       currentTaskEnd: this.state.currentTaskEnd === 0 ? this.state.markerX + length : this.state.currentTaskEnd + length,
-      focuseMode: !this.state.focuseMode,
+      focusMode: !this.state.focusMode,
       mode: type,
       taskTimer: intervalId,
+      tasksLog: tasksLog,
     });
+
+    localStorage.setItem('logs', JSON.stringify(this.state.tasksLog));
   }
 
-  onClickStopFocusMode() {
-    if(this.state.focuseMode) {
-      clearInterval(this.state.taskTimer);
+  stopTimer() {
+    const { focusMode, taskTimer } = this.state;
 
-      //TODO stop current task on curren time
+    if(focusMode) {
+      clearInterval(taskTimer);
+
+      //TODO stop current task on current time
       this.setState({
-        focuseMode: !this.state.focuseMode,
+        focusMode: !focusMode,
         currentTask: null,
         taskTimer: null,
-        tasksLog: [
-          ...this.state.tasksLog,
-          this.state.currentTask
-        ]
       });
+
+      document.title = 'Laegato';
     }
   }
 
@@ -102,7 +120,7 @@ class App extends Component {
     const { key } = e;
 
     if(key === 'Escape') {
-      this.onClickStopFocusMode();
+      this.stopTimer();
     }
   }
 
@@ -110,6 +128,18 @@ class App extends Component {
     this.setState({
       slider: e.target.value
     });
+  }
+
+  toggleTheme() {
+    const { nightMode } = this.state;
+
+    if(nightMode) {
+      document.body.classList.remove('dark');
+    } else {
+      document.body.classList.add('dark');
+    }
+
+    this.setState({nightMode: !nightMode});
   }
 
   componentDidMount() {
@@ -131,10 +161,6 @@ class App extends Component {
 
     setInterval(() => {
       this.setState((prevState) => {
-        // if(prevState.currentTask &&  prevState.markerX >= prevState.currentTask.start + prevState.currentTask.length) {
-        //   this.onClickStopFocusMode();
-        // }
-
         const currentDateTime = new Date();
         const hours = currentDateTime.getHours();
         const minutes = currentDateTime.getMinutes();
@@ -159,22 +185,26 @@ class App extends Component {
   }
 
   render() {
+    const { tasksLog } = this.state;
+    const key = taskKey(new Date());
+    const tasksToday = tasksLog[key] || null;
+
     return (
       <div className='vfull'>
-        <nav className='navbar is-fixed-top' role='navigation' aria-label='main navigation'>
-          <div className={this.state.focuseMode ? 'navbar-brand invisible' : 'navbar-brand'}>
-            <a className='navbar-item' href='https://bulma.io'>
+        <nav className='navbar is-fixed-top' aria-label='main navigation'>
+          <div className={this.state.focusMode ? 'navbar-brand invisible' : 'navbar-brand'}>
+            <a className='navbar-item' href='https://laegato.com'>
               <img src='/img/logo-laegato.png' alt='' width='110' />
             </a>
             {/* <span className='is-size-4 has-text-weight-bold' style={{margin: 'auto'}}>Laegato</span> */}
           </div>
           <div className="navbar-menu">
-            <div className={this.state.focuseMode ? 'navbar-start invisible' : 'navbar-start'}>
+            <div className={this.state.focusMode ? 'navbar-start invisible' : 'navbar-start'}>
             </div>
             <div style={{margin: 'auto', display: 'flex', alignItems: 'strect'}}>
               {/* <div>4,999 online users</div> */}
             </div>
-            <div className={this.state.focuseMode ? 'navbar-end invisible' : 'navbar-end'}>
+            <div className={this.state.focusMode ? 'navbar-end invisible' : 'navbar-end'}>
               <div className='navbar-item'>
                 <div className='field is-grouped'>
                   <p className='control'>
@@ -194,29 +224,37 @@ class App extends Component {
           <div className='columns is-vcentered vfull'>
             <div className='column columns is-multiline'>
               <div className='column is-12'>
-                <h1 className={!this.state.focuseMode ? 'is-size-2 has-text-weight-bold' : 'is-size-2 has-text-weight-bold hide'}>You have {this.state.remainingHours} hours, {this.state.remainingMins} minutes. Enjoy the day.</h1>
-                {this.state.currentTask && <h1 className={this.state.focuseMode ? 'is-size-2 has-text-weight-bold' : 'is-size-2 has-text-weight-bold hide'}>You have {Math.trunc(this.state.currentTask.remainingSec / 60)}  minutes {this.state.currentTask.remainingSec % 60} seconds to #{this.state.mode}</h1>}
+                <h1 className={!this.state.focusMode ? 'is-size-2 has-text-weight-bold' : 'is-size-2 has-text-weight-bold hide'}>
+                You have {this.state.remainingHours} hours, {this.state.remainingMins} minutes. Enjoy the day.
+                </h1>
+                {this.state.currentTask && 
+                <h1 className={this.state.focusMode ? 'is-size-2 has-text-weight-bold' : 'is-size-2 has-text-weight-bold hide'}>
+                You have {Math.trunc(this.state.currentTask.remainingSec / 60)} minutes {this.state.currentTask.remainingSec % 60} seconds to #{this.state.mode}</h1>}
               </div>
               <div className='column is-12'>
-                <svg viewBox='0 0 864 30'>
+                <svg viewBox='0 0 864'>
                   <TimeBar />
-                  {this.state.tasksLog && this.state.tasksLog.map(task => <TaskBar length={task.length} fill={task.color} start={task.start} />)}
-                  {this.state.currentTask && <TaskBar length={this.state.currentTask.length} fill={this.state.currentTask.color} start={this.state.currentTask.start} />}
-                  <rect x={this.state.markerX} y='1' width='0.75' height='30' fill='#212529' />
+                  {tasksToday && 
+                    tasksToday.map(task => <TaskBar length={task.length} fill={task.color} start={task.start} />)}
+                  {this.state.currentTask && 
+                    <TaskBar length={this.state.currentTask.length} fill={this.state.currentTask.color} start={this.state.currentTask.start} />}
+                  <rect x='0' y='5' width='864' height='20' fill='#339AF0' />
+                  {/* <rect x={this.state.markerX} y='1' width='0.75' height='30' fill='#212529' /> */}
+                  <rect x='0' y='5' width='864' height='40' fill='#339AF0' />
                 </svg>
               </div>
               <div className='column is-12' style={{ minHeight: '75px'}}>              
-                <button className={!this.state.focuseMode ? 'button is-outlined btn-tasks btn-work' : 'button is-outlined btn-tasks btn-work hide'} 
+                <button className={!this.state.focusMode ? 'button is-outlined btn-tasks btn-work' : 'button is-outlined btn-tasks btn-work hide'} 
                 data-type='work' onClick={this.startTime}>#work</button>
-                <button className={!this.state.focuseMode ? 'button is-outlined btn-tasks btn-play' : 'button is-outlined btn-tasks btn-play hide'} 
+                <button className={!this.state.focusMode ? 'button is-outlined btn-tasks btn-play' : 'button is-outlined btn-tasks btn-play hide'} 
                 data-type='play' onClick={this.startTime}>#play</button>
-                <button className={!this.state.focuseMode ? 'button is-outlined btn-tasks btn-learn' : 'button is-outlined btn-tasks btn-learn hide'} 
+                <button className={!this.state.focusMode ? 'button is-outlined btn-tasks btn-learn' : 'button is-outlined btn-tasks btn-learn hide'} 
                 data-type='learn' onClick={this.startTime}>#learn</button>
-                <button className={!this.state.focuseMode ? 'button is-outlined btn-tasks btn-break' : 'button is-outlined btn-tasks btn-break hide'} 
+                <button className={!this.state.focusMode ? 'button is-outlined btn-tasks btn-break' : 'button is-outlined btn-tasks btn-break hide'} 
                 data-type='break' onClick={this.startTime}>#break</button>
-                <div className={this.state.focuseMode ? 'focus-controls' : 'hide'}>
+                <div className={this.state.focusMode ? 'focus-controls' : 'hide'}>
                   <div style={{display: 'flex', alignItems: 'center'}}>
-                    <span onClick={this.onClickStopFocusMode} className='icon stop'><i className='ion-ionic ion-md-close'></i></span>
+                    <span onClick={this.stopTimer} className='icon stop'><i className='ion-ionic ion-md-close'></i></span>
                     <span className='is-size-5 has-text-weight-bold' style={{ color: '#000000', opacity: '0.54' }}>Press "Esc" to stop</span>
                   </div>
                   <div className='is-size-5 has-text-weight-bold' style={{display: 'flex', alignItems: 'center', color: '#000000', opacity: '0.54'}}>
@@ -227,7 +265,7 @@ class App extends Component {
             </div>
           </div>
         </section>
-        <footer className={this.state.focuseMode ? 'footer invisible' : 'footer'}>
+        <footer className={this.state.focusMode ? 'footer invisible' : 'footer'}>
           <div className='content' style={{display: 'flex', justifyContent: 'space-between', alignItems: 'flex-end'}}>
             <div>
               Copyright {new Date().getFullYear()} <strong>Godspeed</strong>. All rights reserverd.
@@ -235,10 +273,12 @@ class App extends Component {
             <div className='has-text-weight-bold is-size-5' style={{width: '240px'}}>
               <div>{this.state.slider} minutes</div>
               {/* <Slider onChange={this.onChangeSliderValue} slider={this.state.slider} min='5' max='90' /> */}
-              <input className='slider is-fullwidth' onChange={this.onChangeSliderValue} value={this.state.slider} min='5' max='90' step='1' type='range' />
+              <input className='slider is-fullwidth' 
+              onChange={this.onChangeSliderValue} 
+              value={this.state.slider} min='5' max='90' step='1' type='range' />
             </div>
             <div>
-              <button className='button btn-circle' style={{ marginRight: '1rem' }}></button>
+              <button onClick={this.toggleTheme} className='button btn-circle' style={{ marginRight: '1rem' }}></button>
               <a className="icon button" href='#' style={{color: '#ffffff', backgroundColor: '#212529'}}>
                 <i className='ion-ionic ion-md-help'></i>
               </a>
@@ -262,6 +302,9 @@ const TimeBar = (props) => {
   );
 }
 
+const taskKey = (date) => {
+  return date.toISOString().slice(0, 10).replace(/-/g, '');
+}
 
 
 export default App;
