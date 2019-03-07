cca74fffb5f2407da44ba539ca266a894c72115d
Marieto Ortillan <marietod.ortillan@pccw.com>
2019-03-07 16:24:16 +0800

    Fix structure of defined sagas

--- src/stores/main.js
+++ src/stores/main.js
@@ -1,5 +1,19 @@
-import { applyMiddleware, createStore } from 'redux'
+import { applyMiddleware, createStore, compose } from 'redux'
 import logger from 'redux-logger'
+import createSagaMiddleware from 'redux-saga'
 import { reducers } from '../reducers/root'
+import { sagas } from '../sagas/root'
 
-export const store = createStore(reducers, applyMiddleware(logger))
+const sagaMiddleware  = createSagaMiddleware()
+
+const store = createStore(
+  reducers, 
+  compose (
+    applyMiddleware(sagaMiddleware), 
+    applyMiddleware(logger)
+  )
+)
+
+sagaMiddleware.run(sagas)
+
+export { store }
